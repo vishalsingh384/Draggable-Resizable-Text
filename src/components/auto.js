8de303277ad775d657cdf4aac3118bc0b4732fa3
vishalsingh384 <vishalsing384@gmail.com>
2023-09-04 14:04:22 +0530

    :lipstick: bg width and heigth updated

--- src/components/auto.js
+++ src/components/auto.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect, useRef } from 'react';
 import Draggable from 'react-draggable';
 
 const TextMovable = ({ text, bg}) => {
-  console.log(bg);
+  // console.log(bg);
   const [inputValue, setInputValue] = useState(text);
   const [textAreaClicked, setTextAreaClicked]=useState(false);
 
   const inputRef = useRef(null);
   const inputParentRef=useRef(null);
-  const maxWidth = 700; 
-  const maxHeight=500;
+  const maxWidth = window.innerWidth-20; 
+  const maxHeight=window.innerHeight-20;
   
   useEffect(() => {
     adjustInputSize();
@@ -41,16 +41,17 @@ const TextMovable = ({ text, bg}) => {
     if (inputRef.current) {
       console.log(inputRef.current);
       const input = inputRef.current;
-      // const fontSize=window.getComputedStyle(input).getPropertyValue('font-size');
       const inputParent=inputParentRef.current;
+
       input.style.width = 'auto'; 
       input.style.width = `${Math.min(inputValue.length*24+15, maxWidth)}px`; 
       input.style.height = 'auto'; 
       input.style.height = `${Math.min(input.scrollHeight, maxHeight)}px`;
+
       inputParent.style.width = 'auto'; 
-      inputParent.style.width = `${Math.min(inputValue.length*28+20, maxWidth+20)}px`;
+      inputParent.style.width = `${Math.min(inputValue.length*28+20, maxWidth)}px`;
       inputParent.style.height = 'auto'; 
-      inputParent.style.height = `${Math.min(input.scrollHeight+20, maxHeight+20)}px`;
+      inputParent.style.height = `${Math.min(input.scrollHeight+20, maxHeight)}px`;
     }
   };
 
